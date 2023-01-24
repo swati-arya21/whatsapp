a99403e2bdb522f93a7a991ad369b237f6f873e5
swati-arya21 <swatiarya21899@gmail.com>
2023-01-24 05:05:36 -0800

    first commit

---
+++ whatsapp-swati/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDpD4tb1O2ZxqMk_XWlj-6HFpa_MIyaSXg",
+    authDomain: "whatsapp-swati.firebaseapp.com",
+    projectId: "whatsapp-swati",
+    storageBucket: "whatsapp-swati.appspot.com",
+    messagingSenderId: "869710107232",
+    appId: "1:869710107232:web:2f0e4aef26876bbb32e905",
+    measurementId: "G-MVYL6Z4J3Y"
+  };
+  
